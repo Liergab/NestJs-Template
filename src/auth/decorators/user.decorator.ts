130a722aa14d6a8af0657b382e5d41986de1de8e
liergab <bryangabrielberja25@gmail.com>
2025-05-20 17:55:02 +0800

    Remove unused authentication-related files
    - Deleted AuthMiddleware, RequestWithUser, JwtPayload interfaces, and GetUser decorator to streamline the authentication module and eliminate redundancy.

--- src/auth/decorators/user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { RequestWithUser } from '../interfaces/request.interface';
-
-export const GetUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
-    return request.user;
-  },
-);
